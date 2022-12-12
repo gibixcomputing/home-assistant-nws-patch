eb1322540050b6d0330e92f366d5112ab59b22e6
Joshua Harley <gibwar@gibixonline.com>
2022-12-11 21:22:21 -0700

    Support removal without HA restart

--- custom_components/nws_patch/__init__.py
+++ custom_components/nws_patch/__init__.py
@@ -4,15 +4,20 @@ from __future__ import annotations
 from collections import defaultdict
 from datetime import datetime
 import logging
-from typing import Any, TypedDict, cast
+from typing import TYPE_CHECKING, Any, Callable, TypedDict, cast
 
 from homeassistant.components.weather import (
     ATTR_FORECAST_NATIVE_TEMP,
     ATTR_FORECAST_NATIVE_TEMP_LOW,
 )
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 from homeassistant.util.dt import parse_datetime
+
+if TYPE_CHECKING:
+    from homeassistant.components.nws.weather import NWSWeather
+    from homeassistant.components.weather import WeatherEntity
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -35,6 +40,10 @@ class NewForecast(TypedDict):
     native_templow: float
 
 
+NWS_FORECAST_PROP: Callable[[NWSWeather], Any] | None = None
+NWS_STATE_ATTRIBUTE_PROP: Callable[[WeatherEntity], Any] | None = None
+
+
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Patch NWS forecast and state_attributes methods on setup."""
 
@@ -45,6 +54,15 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     _LOGGER.info("getting ready to patch NWSWeather")
     from homeassistant.components.nws.const import DAYNIGHT
     from homeassistant.components.nws.weather import NWSWeather
+
+    # only copy out the props if we haven't prior, preventing a potential mixup
+    global NWS_FORECAST_PROP
+    if NWS_FORECAST_PROP is None:
+        NWS_FORECAST_PROP = NWSWeather.forecast
+
+    global NWS_STATE_ATTRIBUTE_PROP
+    if NWS_STATE_ATTRIBUTE_PROP is None:
+        NWS_STATE_ATTRIBUTE_PROP = NWSWeather.state_attributes
 
     class NWSWrap(NWSWeather):
         detailed_forecast: property
@@ -64,15 +82,17 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     ).setter(set_detailed_forecast)
     _LOGGER.debug("added detailed forecast property")
 
-    nws_forecast = NWSWeather.forecast
-
     def daily_forecast(self: NWSWrap) -> list[NewForecast] | list[NWSForecast] | None:
         """Convert the normal forecast to a daily forecast."""
 
         _LOGGER.debug("running for mode %s", self.mode)
 
+        if NWS_FORECAST_PROP is None:
+            _LOGGER.error("NWS forecast property has gone missing! :(")
+            return cast(list[NWSForecast], [])
+
         # get the original forecast, and if none return none
-        orig_forecast: list[NWSForecast] | None = nws_forecast.__get__(self)
+        orig_forecast: list[NWSForecast] | None = NWS_FORECAST_PROP.__get__(self)
         if not orig_forecast:
             return None
 
@@ -99,13 +119,18 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
             if len(fcasts) == 1:
                 tomorrow = True
                 updated = {**fcasts[0]}
+                title = "Day"
                 if not updated["daytime"]:
+                    title = "Night"
                     updated[ATTR_FORECAST_NATIVE_TEMP_LOW] = updated[
                         ATTR_FORECAST_NATIVE_TEMP
                     ]
                     del updated[ATTR_FORECAST_NATIVE_TEMP]
 
                 del updated["daytime"]
+                updated[
+                    "detailed_description"
+                ] = f"### {title}\n{cast(NWSForecast, updated)['detailed_description'].strip()}"
 
                 forecast.append(cast(NewForecast, updated))
             elif len(fcasts) == 2:
@@ -141,10 +166,12 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     _LOGGER.info("patching forecast")
     NWSWeather.forecast = property(daily_forecast)  # type: ignore[assignment]
 
-    nws_state_attributes = NWSWeather.state_attributes
-
     def add_detailed_description_state(self: NWSWrap) -> dict[str, Any]:
-        state: dict[str, Any] = nws_state_attributes.__get__(self)
+        if NWS_STATE_ATTRIBUTE_PROP is None:
+            _LOGGER.error("nws state attribute prop has gone missing :(")
+            return {}
+
+        state: dict[str, Any] = NWS_STATE_ATTRIBUTE_PROP.__get__(self)
 
         if self.mode == DAYNIGHT:
             state["detailed_forecast"] = self.detailed_forecast
@@ -166,3 +193,20 @@ async def async_setup_entry(hass: HomeAssistant, config: ConfigType) -> bool:
     del hass
     del config
     return True
+
+
+async def async_remove_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> None:
+    """Restore original NWS forecast behavior on removal."""
+
+    # remove unused required parameters
+    del hass
+    del config_entry
+
+    _LOGGER.info("removing nws forecast patch")
+
+    from homeassistant.components.nws.weather import NWSWeather
+
+    NWSWeather.forecast = NWS_FORECAST_PROP  # type: ignore[assignment]
+    NWSWeather.state_attributes = NWS_STATE_ATTRIBUTE_PROP  # type: ignore[assignment, misc]
+
+    _LOGGER.info("removed nws forecast path :c")
